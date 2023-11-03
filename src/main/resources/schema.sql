c8122c02084642945ab869f17ba538d1d3f7b308
Achinda <achindamihiruk@gmail.com>
2023-11-04 02:19:15 +0530

    Create MySQL database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE student (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(400) NOT NULL
+);
+
+CREATE TABLE picture (
+    student_id INT PRIMARY KEY,
+    path VARCHAR(1000) NOT NULL,
+    CONSTRAINT fk_picture FOREIGN KEY (student_id) REFERENCES student (id) ON DELETE CASCADE
+);
